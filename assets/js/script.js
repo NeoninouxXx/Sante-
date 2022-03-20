c65ca94d4222d3dadb62b7a055f6ee365577718b
xavier turpin <turpin182@gmail.com>
2022-03-20 23:48:46 +0400

    refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,17 @@
-const bar = () => {
-  
-};
-  
-  
-  
-  console.log(bar());
+class enfants {
+  constructor(sexe, nom, prenom) {
+    this.sexe = sexe;
+    this.nom = nom;
+    this.prenom = prenom;
+  }
+  showPrenom(){
+    return console.log(this.prenom);
+  }
+}
+
+const samuel = new enfants ("garcon", "turpin","samuel");
+
+
+const btn = document.querySelector("button");
+
+btn.addEventListener('click',()=>console.log(samuel.showPrenom()));
